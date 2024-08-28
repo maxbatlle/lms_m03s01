c46e3253c114490f92c40dd748a6ddec331a5bf5
Max Batlle <maxbatlle@gmail.com>
2024-08-28 16:37:11 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const pi = 3.14159265359;
+let valorRadio = Number(prompt("Ingrese valor área a calcular"));
+let area = pi*(valorRadio ** 2);
+
+console.log();
+console.log(area); 
+
+document.write("El área del círculo es: ")
+document.write(area)
